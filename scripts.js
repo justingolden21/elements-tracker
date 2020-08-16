14122a7117169992fcbaf08dee255208e1ea3f09
Justin Golden <justingolden21@gmail.com>
2020-08-15 20:54:27 -0700

    Init Vue

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
 $( ()=> {
 
+	new Vue({
+		el: '#app',
+		data: {
+			players: [
+				{name: 'Justin', life: 20, gems: 0},
+				{name: 'Bobby', life: 17, gems: 2},
+				{name: 'Dad', life: 12, gems: 1},
+			],
+		},
+		components: {
+			player: {
+				template: `
+					<div class="border p-2 m-2">
+						<input v-model="name" type="text" class="form-control text-center">
+						Life:
+						<input v-model="life" type="number" value="20" class="form-control">
+						Gems:
+						<input v-model="gems" type="number" value="0" class="form-control">
+					</div>
+				`,
+				props:
+					['name', 'life', 'gems'],
+			},
+		},
+	});
+
 });
