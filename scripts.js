53ad881962e55a791dfba579394b934531ae28b8
Justin Golden <justingolden21@gmail.com>
2020-08-15 21:48:05 -0700

    fix vue error

--- scripts.js
+++ scripts.js
@@ -4,24 +4,39 @@ $( ()=> {
 		el: '#app',
 		data: {
 			players: [
-				{name: 'Justin', life: 20, gems: 0},
-				{name: 'Bobby', life: 17, gems: 2},
-				{name: 'Dad', life: 12, gems: 1},
+				{id: 0, name: 'Justin', life: 20, gems: 0},
+				{id: 1, name: 'Bobby', life: 17, gems: 2},
+				{id: 2, name: 'Dad', life: 12, gems: 1},
 			],
+			nextId: 3,
+		},
+		methods: {
+			handleChange(id, name, life, gems) {
+				console.log(id, name, life, gems);
+				let pos = this.players.map( (elm)=> elm.id).indexOf(id);
+				this.players[pos].name = name;
+				this.players[pos].life = life;
+				this.players[pos].gems = gems;
+			},
 		},
 		components: {
 			player: {
 				template: `
-					<div class="border p-2 m-2">
-						<input v-model="name" type="text" class="form-control text-center">
+					<div class="border shadow p-2 m-2">
+						<input :value="name" type="text" class="form-control text-center" @input="oninput">
 						Life:
-						<input v-model="life" type="number" value="20" class="form-control">
+						<input :value="life" type="number" class="form-control" @input="oninput">
 						Gems:
-						<input v-model="gems" type="number" value="0" class="form-control">
+						<input :value="gems" type="number" class="form-control" @input="oninput">
 					</div>
 				`,
 				props:
-					['name', 'life', 'gems'],
+					['id', 'name', 'life', 'gems'],
+				methods: {
+					oninput() {
+						this.$emit('update-input', this.id, this.name, this.life, this.gems);
+					},
+				},
 			},
 		},
 	});
