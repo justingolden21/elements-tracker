a65f7b1b7d3320453d976f1ce44296de72bc283a
Justin Golden <justingolden21@gmail.com>
2020-08-15 23:53:31 -0700

    add and remove players, show dead players and alive players

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 $( ()=> {
-
 	new Vue({
 		el: '#app',
 		data: {
@@ -9,6 +8,23 @@ $( ()=> {
 				{name: 'Dad', life: 12, gems: 1},
 			],
 		},
+		methods: {
+			removePlayer: function(player) {
+				this.players.splice(this.players.indexOf(player), 1);
+			},
+			addPlayer: function() {
+				this.players.push({name: 'Joe', life: 20, gems: 0});
+			},
+		},
+		computed: {
+			numAlive: function() {
+				return this.players.filter( player => player.life > 0 ).length;
+			},
+		},
+		filters: {
+			pluralize: function(n) {
+				return n === 1 ? 'player' : 'players';
+			},
+		},
 	});
-
 });
